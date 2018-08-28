1d697431093a47d0b5e5ed5985d63d5e03c16a48
Dannyman3819 <danielb2cool@gmail.com>
2018-08-27 23:18:54 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,266 @@
+import listparser as lp
+from bs4 import BeautifulSoup as bs
+import youtube_dl
+import os, shutil
+import string
+import glob
+import traceback
+import json
+from distutils.dir_util import copy_tree
+import datetime
+import time
+import sys
+from pprint import pprint
+
+# Support for both python 2 and 3
+if sys.version_info[0] == 3:
+	from urllib.request import urlopen
+	from urllib import request
+else:
+	from urllib import urlopen
+	import urllib as request
+
+#GLOBAL VARS
+START_HOUR = 0
+NUM_VIDEOS = 0
+DESTINATION_FOLDER = ""
+API_KEY = ""
+DELAY = 0
+
+configFile = 'data/config'
+logFileName = "data/log.txt"
+
+
+def load_configs(configFile):
+	global START_HOUR
+	global NUM_VIDEOS
+	global DESTINATION_FOLDER
+	global API_KEY
+
+	with open(configFile) as f:
+		for line in f:
+			line = line.rstrip() # remove newline
+			if line.find("START_HOUR") != -1:
+				data = line.split("=")
+				START_HOUR = int(data[1])
+			elif line.find("NUM_VIDEOS") != -1:
+				data = line.split("=")
+				NUM_VIDEOS = int(data[1])
+			elif line.find("DESTINATION_FOLDER") != -1:
+				data = line.split("=")
+				DESTINATION_FOLDER = str(data[1])
+			elif line.find("API_KEY") != -1:
+				data = line.split("=")
+				API_KEY = str(data[1])
+			elif line.find("DELAY") != -1:
+				data = line.split("=")
+				DELAY = int(data[1])
+
+
+def get_icons(channel, chid, overwrite=False):
+	if len(channel) == 0:
+		#Skip check for icon file if we are doing a single channel
+		destinationDir = destinationFolder + channel + '/'
+		if not os.path.exists(destinationDir):
+			print("Creating Source Directory")
+			os.makedirs(destinationDir)
+		try:
+			print("Downloading new icon for poster")
+			url_data = urlopen(
+				'https://www.googleapis.com/youtube/v3/channels?part=snippet&id='
+				+ chid + '&fields=items%2Fsnippet%2Fthumbnails&key=' + api_key)
+			data = url_data.read()
+			data = json.loads(data.decode('utf-8'))
+			icon_url = data['items'][0]['snippet']['thumbnails']['high']['url']
+			with open("poster.jpg", 'wb') as f:
+				f.write(request.urlopen(icon_url).read())
+
+			print("Moving Icon...")
+			shutil.move('poster.jpg', destinationDir + 'poster.jpg')
+		except Exception as e:
+			print("An error occured moving Icon")
+			print("Error dump in error.log")
+			with open('error.log', 'a+') as f:
+				f.write(str(datetime.now()) + '\n')
+				f.write(str(e))
+				f.write(traceback.format_exc())
+				f.write("\n\n----------VAR DUMP--------\n\n")
+				pprint(globals(), stream=f)
+				pprint(locals(), stream=f)
+	else:
+		for j in range(0, len(channel)):
+			destinationDir = DESTINATION_FOLDER + channel[j] + ' [Youtube-' + chid[j] + ']/'
+			if not os.path.exists(destinationDir + 'poster.jpg') or overwrite:
+				if not os.path.exists(destinationDir):
+					print("Creating Source Directory")
+					os.makedirs(destinationDir)
+				try:
+					print("Downloading new icon for poster: " + channel[j] + " | " + chid[j])
+					url_data = urlopen(
+						'https://www.googleapis.com/youtube/v3/channels?part=snippet&id='
+						+ chid[j] + '&fields=items%2Fsnippet%2Fthumbnails&key=' + api_key)
+					data = url_data.read()
+					data = json.loads(data.decode('utf-8'))
+					icon_url = data['items'][0]['snippet']['thumbnails']['high']['url']
+					with open("poster.jpg", 'wb') as f:
+						f.write(request.urlopen(icon_url).read())
+
+					print("Moving Icon...")
+					shutil.move('poster.jpg', destinationDir + 'poster.jpg')
+				except Exception as e:
+					print("An error occured moving Icon")
+					print("Error dump in error.log")
+					with open('error.log', 'a+') as f:
+						f.write(str(datetime.now()) + '\n')
+						f.write(str(e))
+						f.write(traceback.format_exc())
+						f.write("\n\n----------VAR DUMP--------\n\n")
+						pprint(globals(), stream=f)
+						pprint(locals(), stream=f)
+			print()
+
+def main():
+	global START_HOUR
+	while True:
+		now = datetime.datetime.now()
+
+		if START_HOUR == 0:
+			START_HOUR = now.hour
+
+		if now.hour == START_HOUR:      # Run once a day at this hour
+			data = lp.parse("data/youtubeData.xml")
+
+			# init for usage outside of this for loop
+			xmltitle = [None] * len(data.feeds)
+			xmlurl = [None] * len(data.feeds)
+			channelIDlist = [None] * len(data.feeds)
+			valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+
+			for i in range(0, len(data.feeds)):
+				xmltitle[i] = data.feeds[i].title       #channel Title
+				xmlurl[i] = data.feeds[i].url   # formated like 'https://www.youtube.com/feeds/videos.xml?channel_id=CHANNELID'
+				indexofid = xmlurl[i].find("id=")
+				channelIDlist[i] = xmlurl[i][indexofid+3:]
+			get_icons(xmltitle, channelIDlist)
+
+			for i in range(0, len(xmltitle)):  # for every channel
+				print(xmltitle[i])
+
+				url_data = urlopen(xmlurl[i], )
+				url_data = url_data.read()
+				xml = bs(url_data.decode('utf-8'), 'html.parser')
+
+				videoList = xml.find_all('entry')
+				#print(xml.find_all('entry'))
+
+				video_download_count = 0
+				for v in videoList:     # for every video in channel
+					#make sure we only download how many we want
+					if video_download_count < NUM_VIDEOS:
+						skip_download = False
+						video_download_count += 1
+						title = v.title.string
+						url = v.link.get('href')
+
+						id = v.id.string
+						channelID = str(v.find('yt:channelid').contents[0])
+						# See if we already downloaded this
+						logFile = open(logFileName, 'r')
+						logFileContents = logFile.read()
+						logFile.close()
+						if id in logFileContents:
+							print("Video Already downloaded")
+						else:
+							print("Downloading - " + title + "  |  " + id)
+							print("Channel - " + str(xmltitle[i]) + "  |  " + channelID)
+
+							ydl_opts = {
+								'outtmpl': 'Download/%(uploader)s - [' + channelID + ']/%(title)s - [%(id)s].%(ext)s',  # need to put channelid in here because what youtube-dl gives may be incorrect
+								#'simulate': 'true',
+								'writethumbnail': 'true',
+								'forcetitle': 'true',
+								#'postprocessor_args': [
+								#	{'add-metadata': 'true'},
+								#],
+								'format': '248+251/best'
+							}
+							#'format:': '140+137',
+							#'prefer_ffmpeg': 'true',
+							try:
+								with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+									info_dict = ydl.extract_info(url, download=False)
+									video_id = info_dict.get("id", None)
+									video_title = info_dict.get("title", None)
+									video_date = info_dict.get("upload_date", None)
+									uploader = info_dict.get("uploader", None)
+									is_live = info_dict.get("is_live", None)
+									if not is_live:
+										ydl.download([url])
+									else:
+										print("Warning! This video is streaming live, it will be skipped")
+										skip_download = True
+
+							except Exception as e:
+								print("Failed to Download")
+								print("Error dump in error.log")
+								skip_download = True
+								with open('error.log', 'a+') as f:
+									f.write(str(datetime.now()) + '\n')
+									f.write(str(e))
+									f.write(traceback.format_exc())
+									f.write("\n\n----------VAR DUMP--------\n\n")
+									pprint(globals(), stream=f)
+									pprint(locals(), stream=f)
+							#if video_title is not None:  # should be the same but just in case
+							#	title = video_title
+							#title = ''.join(c for c in title if c in valid_chars)       # make sure all the chars are valid for windows
+
+							#if glob.glob('*' + video_id + '.mp4'):
+							#	extension = ".mp4"
+							#else:
+							#	extension = ".webm"
+							#title = glob.glob('*' + video_id + '*')[0]
+							#title = title[0:len(title)-5]
+							#title = title[0:len(title)-len(video_id)-1]
+
+
+							#destVideoName = title + " - [" + video_id +"]" + extension
+							#destThumbName = title + " - [" + video_id +"].jpg"
+
+							if not skip_download:
+								sourceDir = 'Download/' + uploader + ' - [' + channelID + ']/'
+								destinationDir = DESTINATION_FOLDER + uploader + ' [Youtube-' + channelID + ']/'
+
+								if not os.path.exists(destinationDir):
+									print("Creating Source Directory")
+									os.makedirs(destinationDir)
+								try:
+									print("Moving Folder...")
+									copy_tree(sourceDir, destinationDir)
+									shutil.rmtree(sourceDir, ignore_errors=True)
+									#shutil.move(videoName, destination + destVideoName)
+									#shutil.move(thumbName, destination + destThumbName)
+
+									#everything was successful so log that we downloaded and moved the video
+									logFile = open(logFileName, 'a')
+									logFile.write(id + ' \n')
+									logFile.close()
+								except Exception as e:
+									print("An error occured moving file")
+									print("Error dump in error.log")
+									with open('error.log', 'a+') as f:
+										f.write(str(datetime.now()) + '\n')
+										f.write(str(e))
+										f.write(traceback.format_exc())
+										f.write("\n\n----------VAR DUMP--------\n\n")
+										pprint(globals(), stream=f)
+										pprint(locals(), stream=f)
+								print()
+				print()
+		print("Going to sleep")
+		time.sleep(DELAY * 60)  
+
+
+if __name__ == "__main__":
+	load_configs(configFile)
+	main()
---
+++ setup.py
@@ -0,0 +1,257 @@
+import sys
+import json
+import os
+import glob
+import pip
+import traceback
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
+
+# support for python 3 and 2
+if sys.version_info[0] == 3:
+	from urllib.request import urlopen
+	from urllib import request
+else:
+	from urllib import urlopen
+	import urllib as request
+
+DEBUGLOGGING = True
+configFile = "data/config"
+
+def logPrint(string):
+	if DEBUGLOGGING:
+		print("DEBUGMSG:" + str(string))
+
+
+def get_input(msg):  # support for python 2 and 3
+	if sys.version_info[0] == 3:
+		d = input(msg)
+	else:
+		d = raw_input(msg)
+	return d
+
+
+def install_dependencies():
+	print("Checking Dependencies....")
+	with open('requirements.txt') as f:
+		try:
+			requirements = f.readlines()
+			for r in requirements:
+				logPrint(r.rstrip())
+			pkg_resources.require(requirements)
+
+		except Exception as e:
+			logPrint("ERROR:\n" + str(e))
+			logPrint(traceback.format_exc())
+			print("One or more dependencies not met!! Please run 'pip install -r requirements.txt'")
+			exit(0)
+
+
+def setup_youtube():
+	api_key = ""
+	setup_not_complete = True
+	while setup_not_complete:
+		print("\n\nSetting up Youtube configs")
+		print("Please goto https://www.youtube.com/subscription_manager")
+		print("On the bottom of the page click 'Export Subscriptions'")
+		print("Put that file in the data directory so it looks like data/subscription_manager.xml")
+		get_input("Click enter to continue.....")
+
+		if os.path.exists('data/subscription_manager.xml'):
+			print("\nFile Found\n\n")
+			setup_not_complete = False
+		else:
+			print("File Not Found!! Please make sure you have it in th correct directory and its named correctly")
+			logPrint("data folder contents:\n" + str(glob.glob("data/*")))
+
+	setup_not_complete = True
+	while setup_not_complete:
+		print("\n\nPlease goto https://www.slickremix.com/docs/get-api-key-for-youtube/")
+		print("Follow this guide to setup an API key you can name the project whatever you want")
+		api_key = get_input("Please enter your API key now:")
+
+		print("Testing key.....")
+		try:
+			url_data = urlopen(
+				'https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&key=' + api_key)
+			code = url_data.getcode()
+			if code != 200:
+				raise Exception('Did not receive webpage')
+
+			print("\nSuccess!")
+			setup_not_complete = False
+			return api_key
+		except Exception as e:
+			logPrint("\n\nERROR\n" + str(e))
+			logPrint(traceback.format_exc())
+			print("Sorry that key did not work!!! Make sure you copied the key correctly")
+
+	return api_key  # shouldn't reach this but just in case
+
+
+def channel_selection():
+	import listparser as lp
+	# This function parses OPML data and allows the user to select which channels to be included
+	print("\n\n\nParsing Youtube data.....")
+	all_channels = False
+	loop = True
+	while loop:
+		selection = get_input(
+			"Would you like to select which channels you want to include, or do you want to include all of them?\n"
+			"If you include all channels you can remove them manually by editing data/youtubeData.xml and deleting the"
+			" entire line of the channel you do not want (Choose this option if you have a lot of subscriptions)\n"
+			"Enter 'all' to keep all subscriptions or 'select' to select which channels (or 'a' or 's'):").lower()
+		if selection == 'all' or selection == 'a':
+			all_channels = True
+			loop = False
+			print("Including all channels")
+		elif selection == 'select' or selection == 's':
+			all_channels = False
+			loop = False
+			print(
+				"You will now be asked to select which channels you would like to include in your download library. \nAny"
+				" channels you do not include will be ignored. \nWarning: if you add a new subscription you must go through this"
+				" process again (until I add a feature to import a channel)\n")
+		else:
+			print("Invalid Selection!!! Try again.")
+
+	file = open("data/youtubeData.xml", 'w')
+	d = lp.parse('data/subscription_manager.xml')
+	l = d.feeds
+	file.write('<opml version="1.1">\n<body>\n')
+	num_channels = len(l)
+	human_count = 1
+
+	for channel in l:
+		title = channel.title
+		url = channel.url
+		print("\nVideo " + str(human_count) + "/" + str(num_channels))
+		# logPrint(title)
+		logPrint(url)
+
+		print(title)
+		if not all_channels:
+			loop = True
+			while loop:
+				selection = get_input(
+					"Select if you want to include the channel above, select 'yes' or 'no' (or 'y' or 'n'):").lower()
+				if selection == 'y' or selection == 'yes':
+					file.write('<outline title="' + title + '" xmlUrl="' + url + '"/>\n')
+					print(title + " will be included!!")
+					loop = False
+				elif selection == 'n' or selection == 'no':
+					loop = False
+					print(title + " will NOT be included!!")
+				else:
+					print("Invalid response. Tray again.")
+		else:
+			file.write('<outline title="' + title + '" xmlUrl="' + url + '"/>\n')
+		human_count += 1
+
+	file.write('</body>\n</opml>')
+	file.close()
+	print("\nComplete.")
+
+
+def setup_config(api_key):
+	print("\n\n\nSetting up Config file")
+	with open(configFile, 'w') as f:
+		f.write("API_KEY=" + api_key + "\n")
+
+		loop = True
+		while loop:
+			selection = get_input("""
+	\nHow would you like the program to run? (x being any number you want)
+		1. Every x minutes
+		2. Every day at x time\n""")
+			if selection == '1':
+				delay_time = get_input("Please enter a number in minutes:")
+				if delay_time.isdigit():
+					print("\nSuccess! It will run every " + delay_time + " minutes")
+					f.write("START_TIME=\n")
+					f.write("DELAY=" + delay_time + '\n')
+					loop = False
+				else:
+					print("\nInvalid response!!! Try again.")
+			elif selection == '2':
+				start_time = get_input("Please enter a number in hours:")
+				if start_time.isdigit():
+					print("\nSuccess! It will run at " + start_time + " everyday!")
+					f.write("START_TIME=" + start_time + 'n')
+					f.write("DELAY=0\n")
+					loop = False
+				else:
+					print("\nInvalid response!!! Try again.")
+			else:
+				print("\nInvalid!!! Please try again.")
+
+		loop = True
+		while(loop):
+			response = get_input("\nHow many videos for each channel do you want to download? (max 15)")
+			if response.isdigit():
+				f.write("NUM_VIDEOS=" + response + '\n')
+				loop = False
+			else:
+				print("\nInvalid!!! Please try again.")
+
+		loop = True
+		while (loop):
+			response = get_input("\nWhere would you like your videos moved? (Usually a Plex library)\n"
+			                     "Make sure you enter the entire address (ex. 'G:\\Plex\\Youtube\\\n')")
+
+			try:
+				if not os.path.isdir(response): # either not a valid directory or not created
+					# try making a directory and see if it throws an exception
+					print("Creating Source Directory")
+					response = os.path.join(response, '')      # add a trailing backslash if not already there
+					os.makedirs(response)
+					f.write("DESTINATION_FOLDER=" + response + '\n')   # if it gets this far we are good
+					print('\nSuccess!')
+					loop = False
+				else:
+					print("Found Source Directory")
+					response = os.path.join(response, '')
+					f.write("DESTINATION_FOLDER=" + response + '\n')
+					print('\nSuccess!')
+					loop = False
+			except Exception as e:
+				logPrint(str(e))
+				print("\nInvalid!!! Please try again.")
+
+
+def main():
+	if not os.path.exists('data/'):
+		os.makedirs('data/')
+	if not os.path.exists('Download/'):
+		os.makedirs('Download/')
+
+	loop = True
+	while loop:
+		print("""
+	    1. First Time Install
+	    2. Channel Selection
+	    3.Exit/Quit
+	    """)
+
+		menuSelection = get_input("What would you like to do? ")
+
+		if menuSelection == "1":
+			install_dependencies()
+			api_key = setup_youtube()
+			channel_selection()
+			setup_config(api_key, )
+			print('\n\n\n----------This completes the setup you may now exit-----------\n')
+		elif menuSelection == "2":
+			channel_selection()
+		elif menuSelection == "3":
+			print("\n Goodbye")
+			exit()
+		else:
+			print("\n Not Valid Choice Try again")
+
+
+if __name__ == "__main__":
+	main()
+	# print(setup_youtube())
+	#channel_selection()
+	#setup_config("123")
